0c54bf15b3f505128eec4d4a1a50a38b1fd574e4
snowsun <snowsun@ehsure.com>
2025-12-24 10:46:34 +0800

    feat: 🎸 新增 HR鉴权

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,10 +1,13 @@
 import { post } from '@/api'
 
+// 判断是否登陆
+export const isLoginApi = () => post<any>('/wx/mem/userAuth/isLogin', {}, { custom: { skipAuthCheck: true, toast: false } })
+
 // 获取OpenId
 export const getOpenId = (data: any) => post<any>('/wx/mem/userAuth/getOpenId', data)
 
-// 判断是否登陆
-export const isLoginApi = () => post<any>('/wx/mem/userAuth/isLogin', {}, { custom: { skipAuthCheck: true, toast: false } })
+// 华润门户快捷健鉴权接口
+export const hrAuth = (data: any) => post<any>('/wx/mem/userAuth/portal/auth', data)
 
 // 登录
 export const getSession = (data: any) => post<any>('/wx/mem/userAuth/getSession', data)
