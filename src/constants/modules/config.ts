9e77fe79a6f620755a4204de7269d01a6d8d1b29
snowsun <snowsun@ehsure.com>
2026-01-04 16:20:14 +0800

    feat: 🎸 新增 第三方小程序配置

--- src/constants/modules/config.ts
+++ src/constants/modules/config.ts
@@ -4,6 +4,9 @@ export const APP_ID = 'wx027ab4853c647162'
 /** 应用前缀 */
 export const PREFIX = 'dicp'
 
+/** 图片地址 */
+export const IMG_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL
+
 /** 微服务中心地址枚举 */
 export enum CenterService {
   /** 活动中心 */
@@ -13,5 +16,10 @@ export enum CenterService {
   Md = '/crbr-wmhy-act-md-center',
 }
 
-/** 图片地址 */
-export const IMG_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL
+/** 第三方小程序配置 */
+export const THIRD_PARTY_APPS = {
+  MALL: {
+    appId: 'wxc16e25946bdc8599',
+    path: 'pages/home/index',
+  },
+} as const
